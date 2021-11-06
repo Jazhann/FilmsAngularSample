ee0c2db6b7d0f7af35daa56204cdb9ae38d7ab4d
Daniel Rodriguez <danirg@gmail.com>
2021-11-06 19:58:31 +0100

    Styles and components

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,18 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
-import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LayoutComponent } from './layout.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    FooterComponent
+    LayoutComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
-    LayoutRoutingModule
+    LayoutRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
   ],
 })
 export class LayoutModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { Globals } from './globals';
 import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   imports: [
     CommonModule,
     TranslateModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   exports: [
-    TranslateModule
+    TranslateModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class SharedModule {
